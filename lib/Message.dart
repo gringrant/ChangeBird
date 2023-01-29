7c829147fb7e9884f27379a596d16b5fb403f936
Grant Davis <gringrant@gmail.com>
2023-01-29 10:00:01 -0500

    Updated sratch and message

--- lib/Message.dart
+++ lib/Message.dart
@@ -21,4 +21,8 @@ class Message{
             textAlign: TextAlign.right),//TextAlign.left for guest
     );
   }
+
+  bool analyzeMessage(){
+    return true;
+  }
 }
--- lib/Scratch.dart
+++ lib/Scratch.dart
@@ -3,35 +3,6 @@ import './chat.dart';
 import './user.dart';
 import './Message.dart';
 
-/*void main() {
-  runApp(const MyApp());
-}*/
-
-/*class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BigBird',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MsgPage(title: 'Change Bird Home Page'),
-    );
-  }
-}*/
-
 class MsgPage extends StatefulWidget {
   final Chat chat;
   final User currentUser = User("Zoom Meeting");
