f2135e0b66a21173ca8e8f79ff50eb714e6ff745
tweeb_kr8x5ge <andelisun@gmail.com>
2023-01-29 10:21:33 -0500

    README.md

--- lib/ActivitiesList.dart
+++ lib/ActivitiesList.dart
@@ -2,4 +2,9 @@ import './Message.dart';
 import './user.dart';
 
 User ai = User("Bob");
-Map<String, Message> map = {'mall': Message('Malls around your area: ', ai)};
+Map<String, Message> map = {
+'meijer': Message('Meijers around your area: ', ai),
+  'theater': Message('Theaters around your area: ', ai),
+  'europe': Message('Fun places to visit in Europe', ai)
+};
+
