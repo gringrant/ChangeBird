0b9fbf6fae703a59f7b7501a38bfbee4c3fe1728
tweeb_kr8x5ge <andelisun@gmail.com>
2023-01-29 10:27:49 -0500

    README.md

--- lib/ActivitiesList.dart
+++ lib/ActivitiesList.dart
@@ -5,6 +5,6 @@ User ai = User("Bob");
 Map<String, Message> map = {
 'groceries': Message('Grocery stores around your area: Meijer, Whole Foods, Walmart', ai),
   'theater': Message('Theaters around your area: AMC, Celebration Cinema Lansing, NCG Cinema', ai),
-  'europe': Message('Fun places to visit in Europe: Paris, France, Berlin, Germany, Mdrid, Spain', ai)
+  'europe': Message('Fun places to visit in Europe: Paris, France, Berlin, Germany, Madrid, Spain', ai)
 };
 
